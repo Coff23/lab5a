8aa0332144334c83972d9d8f561f75180bcffc3c
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-03-10 17:22:06 -0600

    sumArray

--- js/app.js
+++ js/app.js
@@ -51,6 +51,7 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
+
 function sumAndMultiply(a, b, c) { //eslint-disable-line
     let sum = a + b + c;
     let multiply = a * b * c;
@@ -77,12 +78,15 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    let sum = testArray[0] + testArray[1] + testArray[2];
+    let sumString = "2,3,4 was passed in as an array of numbers, and 9 is their sum.";
 
+    return [sum, sumString];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
